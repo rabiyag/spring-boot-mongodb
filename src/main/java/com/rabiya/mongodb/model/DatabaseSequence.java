7f4b67b0e11f0ce15f18824aedbb478fe5d506d8
Rabiya Aqeel <rabiyaghouri@ymail.com>
2023-11-05 10:46:16 +0400

    Initial commit

---
+++ src/main/java/com/rabiya/mongodb/model/DatabaseSequence.java
@@ -0,0 +1,16 @@
+package com.rabiya.mongodb.model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "database_sequences")
+public class DatabaseSequence {
+    private String id;
+    private String seq;
+
+}
